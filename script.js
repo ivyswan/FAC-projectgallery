e9f2d1e5cafd3309e1d8c0d8c867115ead4a7baf
Ivy Swan <ivyswan@Ivys-MacBook-Pro.local>
2022-07-19 13:23:37 +0100

    changes loop in script.js

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-
-// const description = document.querySelectorAll('.description');
-
-// button.addEventListener("click", function () {
-//     description.classList.toggle('show-description');
-// });
-
-// const toggleDescription = function () {
-//     description.classList.toggle('show-description');
-// }
-
-// const toggleDescription = function () {
-//     for (let i = 0; i < description.length; i++) {
-//         description[i].classList.toggle('show-description');
-
-//     }
-// }
-
-// for (let i = 0; i < button.length; i++) {
-//     button[i].addEventListener("click", toggleDescription);
-// }
-
-
-
 const button = document.querySelectorAll('.button');
+const description = document.querySelectorAll('.description');
+
 for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", function (event) {
-        event.preventDefault();
-        const buttonclicked = event.target;
-        const description = buttonclicked.nextElementSibling;
-        description.classList.toggle('show-description');
+    button[i].addEventListener("click", function () {
+        description[i].classList.toggle('show-description');
         if (description === 'none') {
             description.classList.toggle('show-description');
         } else {
